42cb68a3d530d7e2f975ccf2a20c5bccea2ec31b
VyTrg <140966315+VyTrg@users.noreply.github.com>
2025-01-17 07:28:06 +0700

    update

---
+++ src/main/java/com/example/chatroom/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.chatroom.controller;
+
+
+import com.example.chatroom.model.User;
+import com.example.chatroom.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
---
+++ src/main/java/com/example/chatroom/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.chatroom.repository;
+
+import com.example.chatroom.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
