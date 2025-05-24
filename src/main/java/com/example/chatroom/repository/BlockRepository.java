e2ccf8fe31878af34ebc7eeb717ff1a900a596d0
DESKTOP-219GS8B\Lenovo <cuongtran23092004@gmail.com>
2025-05-24 12:29:54 +0700

    update BlockRepository

--- src/main/java/com/example/chatroom/repository/BlockRepository.java
+++ src/main/java/com/example/chatroom/repository/BlockRepository.java
@@ -10,4 +10,4 @@ public interface BlockRepository extends JpaRepository<Block, Long> {
     boolean existsByBlockerAndBlocked(User blocker, User blocked);
 
     void deleteByBlockerAndBlocked(User blocker, User blocked);
-}
+}//
